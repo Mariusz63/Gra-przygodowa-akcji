111c012f81d6d7bff2cc3234447e412f01fe4b43
kuniak9977 <kunio9977@wp.pl>
2024-05-31 13:40:17 +0200

    Improved camera movment

--- Gra_przygodowa_akcji/Assets/_Scripts/Camera_Movment.cs
+++ Gra_przygodowa_akcji/Assets/_Scripts/Camera_Movment.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -18,10 +19,16 @@ public class Camera_Movment : MonoBehaviour
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        float mouseScroll = Input.GetAxis("Mouse ScrollWheel") * mouseSensitivity * Time.deltaTime;
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
+        if ((Mathf.Round(transform.localPosition.z) <= -2 && mouseScroll > 0) || (Mathf.Round(transform.localPosition.z) >= -6 && mouseScroll < 0))
+        {
+            transform.localPosition += new Vector3 (mouseScroll * -0.5f, mouseScroll * -0.5f, mouseScroll * 2f);
+        }
+        
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         target.Rotate(Vector3.up * mouseX);
     }
