c55d1ef927215f59037105639feb5ebf6d57f25b
kuniak9977 <kunio9977@wp.pl>
2024-06-21 03:28:44 +0200

    INV 1 iteration
    Pierdole ten ekwipunek

--- Gra_przygodowa_akcji/Assets/_Scripts/InventoryGUI.cs
+++ Gra_przygodowa_akcji/Assets/_Scripts/InventoryGUI.cs
@@ -1,3 +1,4 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,18 +6,34 @@ using UnityEngine;
 public class InventoryGUI : MonoBehaviour
 {
     private InventoryController inventory;
+    private List<GameObject> cellList;
+    [SerializeField]
+    private GameObject gridCell;
 
     public void SetupInventory (InventoryController _inventory){
         this.inventory = _inventory;
+        
     }
     void Start()
+    {
+        this.cellList = new List<GameObject>();
+        SetSizeByLevel(inventory);
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+    public void SetSizeByLevel (InventoryController _inventory){
+        int invLevel = _inventory.inventoryLevel;
+        int [] invSize = {12, 18, 24, 30};
+        int midPointX = 0, midPointY = 0, spacing = 70;
+
+        GameObject clone = Instantiate(gridCell, new Vector3 (1420,540,0f) , Quaternion.identity, transform);
+
+        RectTransform clonetransform = clone.GetComponent<RectTransform>();
+        Debug.Log(clonetransform.anchoredPosition);
     }
 }
